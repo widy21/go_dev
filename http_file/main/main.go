030907d40c607ce4abe3435079c022b59a1c8eb3
huaxiao <huaxiao@jd.com>
2019-09-24 11:24:45 +0800

    按照上传文件名进行上传保存。

--- http_file/main/main.go
+++ http_file/main/main.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -28,15 +29,17 @@ func init() {
 
 func main() {
 	http.HandleFunc("/upload", upload)
-	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	err := http.ListenAndServe(":"+strconv.Itoa(9090), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	log.Println("ListenAndServer port: ", 9090)
+
 }
 
 // 处理/upload 逻辑
 func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
+	log.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
 		crutime := time.Now().Unix()
 		h := md5.New()
@@ -47,21 +50,26 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseMultipartForm(32 << 20)
 		file, handler, err := r.FormFile("uploadfile")
-		// fmt.Fprintln("handler.Filename=%s",file.)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
+		// fmt.Fprintln("handler.Filename=%s",file.)
+		log.Println("handler.Filename=", handler.Filename)
+		arr := strings.Split(handler.Filename, "/")
+		fileName := arr[len(arr)-1]
+		log.Println("after deal, fileName=", fileName)
 		defer file.Close()
 		fmt.Fprintf(w, "%v", handler.Header)
-		// f, err := os.OpenFile("/tmp/upload/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		f, err := os.OpenFile("/tmp/upload/aa.jpg", os.O_WRONLY|os.O_CREATE, 0666)
+		// destFile, err := os.OpenFile("/tmp/upload/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		destFile, err := os.OpenFile(fmt.Sprintf("/tmp/upload/%s", fileName), os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		defer f.Close()
-		io.Copy(f, file)
-		fmt.Println("upload over...")
+		defer destFile.Close()
+		io.Copy(destFile, file)
+		log.Println("upload over...")
 	}
 }
